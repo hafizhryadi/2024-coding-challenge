49750d17c35cf95b249cccfdab18d60a4b2346a7
hafizhryadi <hafizhryadi@gmail.com>
2024-01-25 20:56:44 +0700

    day 4/366

---
+++ 4-smash.js
@@ -0,0 +1,22 @@
+// day 4/366
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
+
+// function smash (words) {
+//     return words.join(' ');
+// };
+
+// cara singkat
+const smash = (words) => words.join(' ');
+
+// cara barbar
+// function smash (words) {
+//     let result = '';
+//     for(let i = 0; i < words.length; i++) {
+//         result += words[i];
+//         if(i != words.length - 1) result += ' ';
+        
+//     }
+//     return result;
+// };
+
+ console.log(smash(["this", "is", "a", "really", "long", "sentence"]))
