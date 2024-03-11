d111711d18ba7fb340b9881635b61a17d5c70005
hafizhryadi <hafizhryadi@gmail.com>
2024-03-11 12:48:41 +0700

    day 20/366

--- 20-Feast.js
+++ 20-Feast.js
@@ -5,7 +5,7 @@
 //     return beast[beast.length-1] === dish[dish.length-1] && beast[0] === dish[0]
 // }
 
-const feast = (beast, dish) => beast.at(0) === dish.at(0) && beast.at(-1) === dish.at(-1);
+const feast = (beast, dish) => beast.at(0) === dish.at(0) && beast.at(-1) === dish.at(-1)
 
 
 console.log(feast("great blue heron", "garlic naan"))
