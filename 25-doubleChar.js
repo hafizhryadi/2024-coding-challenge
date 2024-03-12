26ec6d72a302bda5747c96523c2bd7058ee528f6
hafizhryadi <hafizhryadi@gmail.com>
2024-03-12 13:32:44 +0700

    day 25/366

---
+++ 25-doubleChar.js
@@ -0,0 +1,16 @@
+// day 25/366
+// https://www.codewars.com/kata/56b1f01c247c01db92000076
+
+// function doubleChar(str) {
+//     let result = ''
+//     for (let i = 0; i < str.length; i++) {
+//         result += str[i]
+//         result += str[i]
+//     }
+//     return result
+// }
+
+const doubleChar = (str) => str.split('').map((el) => el + el).join('')
+
+
+console.log(doubleChar("abcd"))
