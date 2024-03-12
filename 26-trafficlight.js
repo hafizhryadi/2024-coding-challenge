5393d668351c60768010dd6027ea159a312cad57
hafizhryadi <hafizhryadi@gmail.com>
2024-03-12 13:49:42 +0700

    day 26/366

---
+++ 26-trafficlight.js
@@ -0,0 +1,50 @@
+// day 26/366
+// https://www.codewars.com/kata/58649884a1659ed6cb000072
+
+// function updateLight(current) {
+  
+//     if (current === 'green') {
+//         return 'yellow'
+//     } else if (current === 'yellow') {
+//         return 'red'
+//     } else {
+//         return 'green'
+//     }
+  
+// }
+
+// function updateLight(current) {
+  
+//     switch (current) {
+//         case 'green': 
+//             return 'yellow'
+//         case 'yellow': 
+//             return 'red'
+//         case 'red': 
+//             return 'green'
+//     }
+  
+// }
+
+const updateLight = (current) => current === 'green' ? 'yellow' : current === 'yellow' ? 'red' : 'green'
+
+// using object
+// const rules = {
+//     green : 'yellow',
+//     yellow : 'red',
+//     red : 'green',
+// }
+
+// function updateLight(current) {
+//   return rules[current]
+// }
+
+// const updateLight = (current) => ({
+//     green : 'yellow',
+//     yellow : 'red',
+//     red : 'green',
+// })[current]
+
+
+
+console.log(updateLight("red"))
