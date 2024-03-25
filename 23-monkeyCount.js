2f762d34c8edb54756fe44752e503f0171387971
hafizhryadi <hafizhryadi@gmail.com>
2024-03-25 14:29:38 +0700

    day 23/366

---
+++ 23-monkeyCount.js
@@ -0,0 +1,15 @@
+// day 23/366
+// https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/javascript
+
+// function monkeyCount(n) {
+//     const result = []
+//     for (let i = 1; i <= n; i++) {
+//         result.push(i)
+//     }
+//     return result
+// }
+
+const monkeyCount = (n) => Array.from({length: n}, (v, i) => i + 1)
+
+
+console.log(monkeyCount(10))
